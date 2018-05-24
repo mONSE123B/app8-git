b3323841abb946b30a23ef2a7412d2b133cac312
MonseChantes <cb16.chantes.morales.monserrat@gmail.com>
2018-05-23 21:01:56 -0500

    4subida
    aplicacion8

--- js/acciones1.js
+++ js/acciones1.js
@@ -2,24 +2,23 @@
 
 $(document).ready(function (e){
 	document.addEventListener("deviceready",function(){
-		$('#disp table td').ep(3).text(device.model);
-		$('#disp table td').ep(5).text(device.cordova);
-		$('#disp table td').ep(7).text(device.platform);
-		$('#disp table td').ep(9).text(device.version);
-		$('#disp table td').ep(11).text(device.uuid);
+	$('#disp table td').eq(3).text(device.model);
+	$('#disp table td').eq(5).text(device.cordova);
+	$('#disp table td').eq(7).text(device.platform);
+	$('#disp table td').eq(9).text(device.version);
+	$('#disp table td').eq(11).text(device.uuid);
 	document.addEventListener("pause",function(){
-		escribehistoria ('la app se pauso');
+		escribehistoria ('la app se pausó');
 	},false);//pause
 	document.addEventListener("resume",function(){
-		escribehistoria ('la app se reinicio');
+		escribehistoria ('la app se reinició');
 	},false);//resume
 	document.addEventListener("online",function(){
-		escribehistoria ('la app se conecto a la red');
+		escribehistoria ('la app se conectó a la red');
 	},false);//se conecto
-	document.addEventsListener("offline",function(){
-		escribehistoria ('la app se desconecto de la red');
-	},false);//se desconecto
-	
+	document.addEventListener("offline",function(){
+		escribehistoria('la app se desconecto de la red');
+	},false);// se desconecto
 	
 	},false);//ready device
 	
@@ -28,6 +27,7 @@ $(document).ready(function (e){
 function escribehistoria(accion){
 	$('#eHistoria').append('<li>'+accion+'</li>');
 }
+	
 
 	
 		
